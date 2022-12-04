a36c510c3a653c79886e7040560bfa4e23daa21e
Yunyi Huang <yuh022@ucsd.edu>
2022-12-04 02:55:02 -0800

    basic structure

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 import pandas as pd 
-from baselinemodel import build_model
+from relevance_bucket_classifier import build_model
 
 if __name__ == '__main__':
     target = sys.argv[1]
